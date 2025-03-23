8e5a2868ca528b511d15292dd7e0adc99ff9945a
hyo141 <hyochang141@gmail.com>
2025-03-23 16:34:11 +0900

    hyochang preparation1

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,40 @@
 import Routers from "./Routers";
+import {
+  createNetworkConfig,
+  SuiClientProvider,
+  WalletProvider,
+} from "@mysten/dapp-kit";
+import { getFullnodeUrl } from "@mysten/sui/client";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+const queryClient = new QueryClient();
+
+const { networkConfig } = createNetworkConfig({
+  localnet: { url: getFullnodeUrl("localnet") },
+  testnet: { url: getFullnodeUrl("testnet") },
+  testnet_suiscan: { url: "https://rpc-testnet.suiscan.xyz:443" },
+  mainnet: { url: getFullnodeUrl("mainnet") },
+});
 
 function App() {
-  return <Routers></Routers>;
+  return (
+    <QueryClientProvider client={queryClient}>
+      <SuiClientProvider
+        networks={networkConfig}
+        defaultNetwork="testnet_suiscan"
+      >
+        <WalletProvider
+          autoConnect
+          stashedWallet={{
+            name: "DOKPAMI2",
+            network: "testnet",
+          }}
+        >
+          <Routers />
+        </WalletProvider>
+      </SuiClientProvider>
+    </QueryClientProvider>
+  );
 }
 
 export default App;
--- src/pages/Main/Main.tsx
+++ src/pages/Main/Main.tsx
@@ -1,8 +1,30 @@
+import { useSuiClientQuery } from "@mysten/dapp-kit";
+
 export default function Main() {
+  const { data, isPending, isError, error } = useSuiClientQuery(
+    "getOwnedObjects",
+    {
+      owner:
+        "0x23c11df86fad8d628fe9b7fb6bf0b27be231f995b476ae1cff2a227575e96fad",
+    },
+    {
+      gcTime: 10000,
+    }
+  );
+
+  if (isPending) {
+    return <div>Loading...</div>;
+  }
+
+  if (isError) {
+    return <div>Error: {error.message}</div>;
+  }
+
   return (
     <div>
       <div className="text-2xl">Blockblock</div>
       <div className="text-lg">여기다 작업해주시면 됩니다.</div>
+      <pre>{JSON.stringify(data, null, 2)}</pre>
     </div>
   );
 }
