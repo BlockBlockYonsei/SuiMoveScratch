f238f14cfbede2b46566b824075272538c349e03
guboc11 <guboc11@gmail.com>
2025-04-05 16:00:24 +0900

    Remove unused import codes & Update package viewer 2 page

--- src/pages/PackageViewer1/Main.tsx
+++ src/pages/PackageViewer1/Main.tsx
@@ -1,7 +1,5 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 import { useSuiClientQuery } from "@mysten/dapp-kit";
-// import { SuiMoveNormalizedType } from "@mysten/sui/client";
-import { parseSuiMoveNormalizedType } from "./utils";
 import { FunctionCard, StructCard } from "./components";
 import {
   SuiMoveNormalizedModule,
--- src/pages/PackageViewer2/Main.tsx
+++ src/pages/PackageViewer2/Main.tsx
@@ -2,6 +2,7 @@ export default function Main() {
   return (
     <div>
       <div className="text-3xl">BlockBlock</div>
+      <div className="text-3xl">여기다 작업해주시면 됩니다.</div>
     </div>
   );
 }
