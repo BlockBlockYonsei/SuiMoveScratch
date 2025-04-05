962a41336315beb04498434bf7df154ee693f000
guboc11 <guboc11@gmail.com>
2025-04-05 16:26:39 +0900

    Add move source code and read and store to sourceCode state

--- src/pages/PackageViewer2/Main.tsx
+++ src/pages/PackageViewer2/Main.tsx
@@ -1,4 +1,19 @@
+import { useEffect, useState } from "react";
+
 export default function Main() {
+  const [sourceCode, setSourceCode] = useState(null);
+
+  useEffect(() => {
+    fetch("/move_source_code.json")
+      .then((res) => res.json())
+      .then((data) => {
+        setSourceCode(data);
+        console.log(data);
+      })
+      .catch((err) => {
+        console.error("Failed to load JSON:", err);
+      });
+  }, []);
   return (
     <div>
       <div className="text-3xl">BlockBlock</div>
