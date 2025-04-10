cadaa3160c096ed113771710d8ddf86f185b0fec
guboc11 <guboc11@gmail.com>
2025-04-10 17:59:02 +0900

    Add NoCodeMove Page

--- src/Routers.tsx
+++ src/Routers.tsx
@@ -2,14 +2,16 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Layout from "./Layout";
 import PackageViewer1 from "./pages/PackageViewer1/Main";
 import PackageViewer2 from "./pages/PackageViewer2/Main";
+import NoCodeMove from "./pages/NoCodeMove/Main";
 
 export default function Routers() {
   return (
     <Router>
       <Routes>
         <Route element={<Layout />}>
-          <Route path="/" element={<PackageViewer2 />} />
+          <Route path="/" element={<NoCodeMove />} />
           <Route path="/packageviewer1" element={<PackageViewer1 />} />
+          <Route path="/packageviewer2" element={<PackageViewer2 />} />
         </Route>
       </Routes>
     </Router>
