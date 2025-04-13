30ae847f8ef2926a9729b857d4e60b0d639bb172
guboc11 <guboc11@gmail.com>
2025-04-13 20:39:06 +0900

    Update imports type & Update structs type and UI details

--- src/pages/NoCodeMove/Main.tsx
+++ src/pages/NoCodeMove/Main.tsx
@@ -1,26 +1,23 @@
 import { useSuiClientQuery } from "@mysten/dapp-kit";
 import {
+  SuiMoveNormalizedField,
   SuiMoveNormalizedModules,
   SuiMoveNormalizedStruct,
 } from "@mysten/sui/client";
 import { useEffect, useRef, useState } from "react";
 
 export default function Main() {
-  const [imports, setImports] = useState<Record<string, string[]>>({});
+  const [imports, setImports] = useState<
+    Record<string, Record<string, SuiMoveNormalizedStruct>>
+  >({});
   const [structs, setStructs] = useState<
     Record<string, SuiMoveNormalizedStruct>
   >({});
 
-  const [array, setArray] = useState<string[]>([]);
-  const [isEditing, setIsEditing] = useState(false);
-  const [inputValue, setInputValue] = useState("");
-  const inputRef = useRef<HTMLInputElement>(null);
+  const CURRENT_PACKAGE =
+    "0x1111111111111111111111111111111111111111111111111111111111111111";
 
-  useEffect(() => {
-    if (isEditing && inputRef.current) {
-      inputRef.current.focus();
-    }
-  }, [isEditing]);
+  const CURRENT_MODULE = "CurrentModule";
 
   const packages = [
     "0x0000000000000000000000000000000000000000000000000000000000000001",
@@ -28,7 +25,6 @@ export default function Main() {
   ];
 
   const PACKAGE =
-    // "0x31323c09dee186fae0b38e0dace096140f5765713e64d10d95f2537b4b699ab4";
     "0x0000000000000000000000000000000000000000000000000000000000000002";
   const { data, isPending, error } = useSuiClientQuery(
     "getNormalizedMoveModulesByPackage",
@@ -58,130 +54,34 @@ export default function Main() {
 
   if (error) return <div>Error: {error?.message || "error"}</div>;
 
+  /// ===============================================================================
+  /// Return of Main()
+  /// ===============================================================================
   return (
     <div className="min-h-screen p-6 max-w-xl mx-auto bg-gray-100">
       <h1 className="text-2xl font-bold">🛠️ No Code 텍스트 에디터</h1>
       <br></br>
       <Imports data={data}></Imports>
-      struct 는 이 로직이 아니야!! data 에서 추가하는 게 아니라 내가 직접 걍
-      추가하는 거라고!
-      <div className="bg-white p-4 rounded-xl border-2 border-black">
-        <div className="inline-block bg-gray-200 text-2xl">Struct</div>
-        {Object.entries(structs).map(([key, value]) => {
-          return (
-            <div>
-              <div>{key}</div>
-              <div>{JSON.stringify(value)}</div>
-            </div>
-          );
-        })}
 
-        {array.map((item, idx) => (
-          <div>
-            public struct{" "}
-            <span className="text-emerald-500 font-semibold">{item}</span>{" "}
-            &#123;
-            <div>&#125;</div>
-          </div>
-        ))}
-        <div>
-          {isEditing && (
-            <input
-              ref={inputRef}
-              value={inputValue}
-              placeholder="Struct Name을 입력하세요."
-              onChange={(e) => setInputValue(e.target.value)}
-              onBlur={() => {
-                // cancelEditing();
-                setInputValue("");
-                setIsEditing(false);
-              }}
-              onKeyDown={(e) => {
-                if (e.key === "Enter") {
-                  // finishEditing();
-                  const trimmed = inputValue.trim();
-                  if (trimmed) {
-                    setArray([...array, trimmed]);
-                  }
-                  setInputValue("");
-                  setIsEditing(false);
-                }
-              }}
-              className="px-3 py-2 border border-gray-300 rounded-xl focus:outline-none"
-            />
-          )}
-        </div>
-      </div>
-      <div>
-        {!isEditing && (
-          <button
-            onClick={() => setIsEditing(true)}
-            className="bg-blue-500 text-white px-4 py-2 rounded-xl hover:bg-blue-600 transition"
-          >
-            ➕ Struct 추가
-          </button>
-        )}
-      </div>
-      <Button
-        onClick={() => {
-          setStructs({
-            MembershipPolicy: {
-              abilities: {
-                abilities: [],
-              },
-              fields: [
-                {
-                  name: "a",
-                  type: {
-                    Struct: {
-                      address: "",
-                      module: "",
-                      name: "",
-                      typeArguments: [],
-                    },
-                  },
-                },
-              ],
-              typeParameters: [
-                {
-                  constraints: {
-                    abilities: [],
-                  },
-                  isPhantom: false,
-                },
-              ],
-            },
-          });
-        }}
-      >
-        ➕ Struct(가짜 데이터) 추가
-      </Button>
+      <Structs data={data}></Structs>
     </div>
   );
 
+  /// ===============================================================================
+  /// Inside Components
+  /// ===============================================================================
   function Imports({ data }: { data: SuiMoveNormalizedModules }) {
     const [isOpen, setIsOpen] = useState(false);
 
-    const addImportBlock = (module: string, i: string) => {
-      console.log(imports[module]);
-      if (imports[module]) {
-        const newImports = [...imports[module], i];
-        setImports((prev) => ({
-          ...prev,
-          [module]: newImports,
-        }));
-      } else {
-        setImports((prev) => ({
-          ...prev,
-          [module]: [i],
-        }));
-      }
-    };
-
-    const handleConfirm = (module: string, i: string) => {
+    const addImport = (module: string, struct: string) => {
       if (module) {
-        console.log(module);
-        addImportBlock(module, i);
+        setImports((prev) => ({
+          ...prev,
+          [module]: {
+            ...(prev[module] || {}),
+            [struct]: data[module].structs[struct],
+          },
+        }));
         setIsOpen(false);
       }
     };
@@ -194,7 +94,7 @@ export default function Main() {
               <div key={key}>
                 <span className="text-blue-500">use</span> sui::{key}:: &#123;{" "}
                 <span className="text-emerald-500 font-semibold">
-                  {values.join(", ")}
+                  {Object.keys(values).join(", ")}
                 </span>{" "}
                 &#125;;
               </div>
@@ -202,9 +102,12 @@ export default function Main() {
           })}
         </div>
         <br></br>
-        <Button onClick={() => setIsOpen((prev) => !prev)}>
+        <button
+          onClick={() => setIsOpen((prev) => !prev)}
+          className="px-4 py-2 my-2 rounded-xl bg-blue-500 cursor-pointer hover:bg-blue-600 text-white transition"
+        >
           ➕ Import 추가
-        </Button>
+        </button>
         <div className="relative">
           {isOpen && (
             <div className="apsolute left-0 p-4 mt-2 bg-white rounded-xl shadow overflow-auto max-h-64">
@@ -220,26 +123,13 @@ export default function Main() {
                         <li
                           key={k}
                           onClick={() => {
-                            handleConfirm(moduleName, k);
-                            setIsOpen(false);
+                            addImport(moduleName, k);
                           }}
                           className="px-4 py-2 text-emerald-500 hover:bg-blue-50 cursor-pointer transition"
                         >
                           {k}
                         </li>
                       ))}
-                      {/* {Object.keys(moduleData.exposedFunctions).map((k) => (
-                      <li
-                        key={k}
-                        onClick={() => {
-                          handleConfirm(moduleName, k);
-                          setIsOpen(false);
-                        }}
-                        className="px-4 py-2 text-pink-500 hover:bg-blue-50 cursor-pointer transition"
-                      >
-                        {k}()
-                      </li>
-                    ))} */}
                     </ul>
                   </li>
                 ))}
@@ -250,16 +140,267 @@ export default function Main() {
       </div>
     );
   }
+
+  function Structs({ data }: { data: SuiMoveNormalizedModules }) {
+    const [isEditing, setIsEditing] = useState(false);
+    const [inputValue, setInputValue] = useState("");
+    const inputRef = useRef<HTMLInputElement>(null);
+
+    useEffect(() => {
+      if (isEditing && inputRef.current) {
+        inputRef.current.focus();
+      }
+    }, [isEditing]);
+
+    return (
+      <div>
+        <div className="bg-white p-4 rounded-xl border-2 border-black">
+          <div className="inline-block bg-gray-200 text-2xl">Struct</div>
+          {/* Structs 하나씩 보여주는 곳 */}
+          {Object.entries(structs).map(([key, value]) => {
+            return (
+              <div>
+                <Struct structName={key} structData={value}></Struct>
+              </div>
+            );
+          })}
+
+          {/* Struct 추가 버튼 클릭 시 입력 필드 */}
+          <div>
+            {isEditing && (
+              <input
+                ref={inputRef}
+                value={inputValue}
+                placeholder="Struct Name을 입력하세요."
+                onChange={(e) => setInputValue(e.target.value)}
+                onBlur={() => {
+                  setInputValue("");
+                  setIsEditing(false);
+                }}
+                onKeyDown={(e) => {
+                  if (e.key === "Enter") {
+                    const trimmed = inputValue.trim();
+                    const newStruct = newEmptyStruct({
+                      packageAddr: CURRENT_PACKAGE,
+                      module: CURRENT_MODULE,
+                      structName: trimmed,
+                    });
+                    if (trimmed) {
+                      setStructs((prev) => ({
+                        ...prev,
+                        [trimmed]: newStruct,
+                      }));
+                    }
+                    setInputValue("");
+                    setIsEditing(false);
+                  }
+                }}
+                className="px-3 py-2 border border-gray-300 rounded-xl focus:outline-none"
+              />
+            )}
+          </div>
+        </div>
+        {/* Struct 추가 버튼 */}
+        <div>
+          {!isEditing && (
+            <button
+              onClick={() => setIsEditing(true)}
+              className="bg-blue-500 text-white px-4 py-2 rounded-xl cursor-pointer hover:bg-blue-600 transition"
+            >
+              ➕ Struct 추가
+            </button>
+          )}
+        </div>
+      </div>
+    );
+  }
+
+  function Struct({
+    structName,
+    structData,
+  }: {
+    structName: string;
+    structData: SuiMoveNormalizedStruct;
+  }) {
+    const [isEditing, setIsEditing] = useState(false);
+    const [inputValue, setInputValue] = useState("");
+    // const [fields, setFields] = useState<string[]>([]);
+    const [fields, setFields] = useState<SuiMoveNormalizedField[]>([]);
+    const inputRef = useRef<HTMLInputElement>(null);
+    const [isOpen, setIsOpen] = useState(false);
+
+    const ABILITIES = ["Copy", "Drop", "Store", "Key"] as const;
+
+    useEffect(() => {
+      if (isEditing && inputRef.current) {
+        inputRef.current.focus();
+      }
+    }, [isEditing]);
+
+    return (
+      <div>
+        <div>
+          public struct{" "}
+          <span className="text-emerald-500 font-semibold">{structName}</span>{" "}
+          {
+            <span>
+              {ABILITIES.map((a) => (
+                <button
+                  onClick={() => {
+                    let newStructData = structData;
+                    if (structData.abilities.abilities.includes(a)) {
+                      newStructData.abilities.abilities =
+                        structData.abilities.abilities.filter(
+                          (ability) => ability !== a
+                        );
+                    } else {
+                      newStructData.abilities.abilities.push(a);
+                    }
+
+                    setStructs((prev) => ({
+                      ...prev,
+                      [structName]: newStructData,
+                    }));
+                  }}
+                  className={`border-2 border-black px-1 rounded-md cursor-pointer ${
+                    structData.abilities.abilities.includes(a)
+                      ? "bg-emerald-300"
+                      : ""
+                  }`}
+                >
+                  {a}
+                </button>
+              ))}{" "}
+            </span>
+          }
+          &#123;
+          {!isEditing && (
+            <span>
+              <button
+                onClick={() => setIsEditing(true)}
+                className="border-2 border-blue-500 px-2 rounded-md cursor-pointer hover:bg-blue-600 transition"
+              >
+                ➕ 필드 추가
+              </button>
+            </span>
+          )}
+        </div>
+        {fields.map((field, idx) => (
+          <div className="relative">
+            <span>{field.name}</span> :{" "}
+            <button
+              onClick={() => {
+                setIsOpen((prev) => !prev);
+              }}
+              className="border-2 border-black cursor-pointer rounded-md"
+            >
+              {field.type.toString()}
+            </button>
+          </div>
+        ))}
+        {isOpen && (
+          <div className="apsolute left-0 p-4 mt-2 bg-white rounded-xl shadow overflow-auto max-h-64">
+            <ul className="w-48 bg-white border rounded-xl shadow-lg z-10">
+              {Object.entries(imports).map(([moduleName, structData]) => (
+                <li key={moduleName} className="relative group">
+                  <div className="px-4 py-2 hover:bg-blue-100 cursor-pointer rounded-xl transition">
+                    {moduleName}
+                  </div>
+
+                  <ul className="absolute left-full top-0 w-40 bg-white border rounded-xl shadow-lg hidden group-hover:block z-20">
+                    {Object.keys(structData).map((structName) => (
+                      <li
+                        key={structName}
+                        onClick={() => {
+                          // addImport(moduleName, k);
+                        }}
+                        className="px-4 py-2 text-emerald-500 hover:bg-blue-50 cursor-pointer transition"
+                      >
+                        {structName}
+                      </li>
+                    ))}
+                  </ul>
+                </li>
+              ))}
+            </ul>
+          </div>
+        )}
+        {isEditing && (
+          <div>
+            <input
+              ref={inputRef}
+              value={inputValue}
+              placeholder="Field Name을 입력하세요."
+              onChange={(e) => setInputValue(e.target.value)}
+              onBlur={() => {
+                setInputValue("");
+                setIsEditing(false);
+              }}
+              onKeyDown={(e) => {
+                if (e.key === "Enter") {
+                  // finishEditing();
+                  const trimmed = inputValue.trim();
+                  if (trimmed) {
+                    // setFields([...fields, trimmed]);
+                    setFields((prev) => [
+                      ...prev,
+                      {
+                        name: trimmed,
+                        type: "U64",
+                      },
+                    ]);
+                  }
+                  setInputValue("");
+                  setIsEditing(false);
+                }
+              }}
+              className="px-3 py-2 border border-gray-300 rounded-xl focus:outline-none"
+            />
+          </div>
+        )}
+        <div>&#125;</div>
+      </div>
+    );
+  }
 }
 
-// ✅ Button 컴포넌트
-function Button({ children, onClick }: { children: any; onClick: any }) {
-  return (
-    <button
-      onClick={onClick}
-      className="px-4 py-2 my-2 rounded-xl bg-blue-500 hover:bg-blue-600 text-white transition"
-    >
-      {children}
-    </button>
-  );
+/// ===============================================================================
+/// Outside Components
+/// ===============================================================================
+
+function newEmptyStruct({
+  packageAddr,
+  module,
+  structName,
+}: {
+  packageAddr: string;
+  module: string;
+  structName: string;
+}): SuiMoveNormalizedStruct {
+  return {
+    abilities: {
+      abilities: [],
+    },
+    fields: [
+      {
+        name: structName,
+        type: {
+          Struct: {
+            address: packageAddr,
+            module: module,
+            name: structName,
+            typeArguments: [],
+          },
+        },
+      },
+    ],
+    typeParameters: [
+      {
+        constraints: {
+          abilities: [],
+        },
+        isPhantom: false,
+      },
+    ],
+  };
 }
