041a23db8c4f647c4ad8055202c1c2480b5fc3dc
guboc11 <guboc11@gmail.com>
2025-04-14 15:14:22 +0900

    Add AddingFunctionButton and InputField

--- src/pages/NoCodeMove/Main.tsx
+++ src/pages/NoCodeMove/Main.tsx
@@ -1,6 +1,6 @@
 import { useSuiClientQuery } from "@mysten/dapp-kit";
 import { SuiMoveNormalizedStruct } from "@mysten/sui/client";
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import Imports from "./_Imports";
 import Structs from "./_Structs";
 
@@ -11,6 +11,20 @@ export default function Main() {
   const [structs, setStructs] = useState<
     Record<string, SuiMoveNormalizedStruct>
   >({});
+
+  //========================== Function State
+
+  const [isEditing, setIsEditing] = useState(false);
+  const [inputValue, setInputValue] = useState("");
+  const inputRef = useRef<HTMLInputElement>(null);
+
+  useEffect(() => {
+    if (isEditing && inputRef.current) {
+      inputRef.current.focus();
+    }
+  }, [isEditing]);
+
+  // =================================================
 
   const packages = [
     "0x0000000000000000000000000000000000000000000000000000000000000001",
@@ -53,11 +67,49 @@ export default function Main() {
       <br></br>
       <Imports data={data} imports={imports} setImports={setImports}></Imports>
 
+      <br></br>
       <Structs
         structs={structs}
         setStructs={setStructs}
         imports={imports}
       ></Structs>
+
+      <br></br>
+      <div>
+        <div className="bg-white p-4 rounded-xl border-2 border-black">
+          <div className="inline-block bg-gray-200 text-2xl">Function</div>
+          {/* Function 추가 버튼 클릭 시 입력 필드 */}
+          <div>
+            {isEditing && (
+              <input
+                ref={inputRef}
+                value={inputValue}
+                placeholder="Function Name을 입력하세요."
+                onBlur={() => {
+                  setInputValue("");
+                  setIsEditing(false);
+                }}
+                onChange={(e) => setInputValue(e.target.value)}
+                onKeyDown={(e) => {
+                  // addStruct(e);
+                }}
+                className="px-3 py-2 border border-gray-300 rounded-xl focus:outline-none"
+              />
+            )}
+          </div>
+        </div>
+        {/* Function 추가 버튼 */}
+        <div>
+          {!isEditing && (
+            <button
+              onClick={() => setIsEditing(true)}
+              className="bg-blue-500 text-white px-4 py-2 my-2 rounded-xl cursor-pointer hover:bg-blue-600 transition"
+            >
+              ➕ Function 추가
+            </button>
+          )}
+        </div>
+      </div>
     </div>
   );
 }
--- src/pages/NoCodeMove/_Structs.tsx
+++ src/pages/NoCodeMove/_Structs.tsx
@@ -97,7 +97,7 @@ export default function Structs({ structs, setStructs, imports }: Props) {
         {!isEditing && (
           <button
             onClick={() => setIsEditing(true)}
-            className="bg-blue-500 text-white px-4 py-2 rounded-xl cursor-pointer hover:bg-blue-600 transition"
+            className="bg-blue-500 text-white px-4 py-2 my-2 rounded-xl cursor-pointer hover:bg-blue-600 transition"
           >
             ➕ Struct 추가
           </button>
