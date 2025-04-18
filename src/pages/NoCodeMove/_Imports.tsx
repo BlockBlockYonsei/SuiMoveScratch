47e14eff516a5dc73ee9ae2ebb6010f317d1a05a
guboc11 <guboc11@gmail.com>
2025-04-18 16:47:41 +0900

    Refactor Imports for better readability

--- src/pages/NoCodeMove/_Imports.tsx
+++ src/pages/NoCodeMove/_Imports.tsx
@@ -30,62 +30,62 @@ export default function Imports({ data, imports, setImports }: Props) {
     }
   };
   return (
-    <div>
-      <div className="bg-white p-4 rounded-xl border-2 border-black">
-        <div className="inline-block bg-gray-200 text-2xl">Imports</div>
-        {Object.entries(imports).map(([key, values]) => {
-          return (
-            <div key={key}>
-              <span className="text-blue-500">use</span> sui::{key}:: &#123;{" "}
-              <span className="text-emerald-500 font-semibold">
-                {Object.keys(values).join(", ")}
-              </span>{" "}
-              &#125;;
-            </div>
-          );
-        })}
-      </div>
-      <div className="relative">
-        <button
-          onClick={() => setIsOpen((prev) => !prev)}
-          onKeyDown={(e) => {
-            if (e.key === "Escape") setIsOpen(false);
-          }}
-          className="px-4 py-2 my-2 rounded-xl bg-blue-500 cursor-pointer hover:bg-blue-600 text-white transition"
-        >
-          ➕ Import 추가
-        </button>
-        {isOpen && (
-          <div className="absolute left-0 p-4 mt-2 w-96 z-50 bg-white rounded-xl shadow overflow-auto max-h-64">
-            <ul className="w-48 bg-white border rounded-xl shadow-lg z-10">
-              {Object.entries(data).map(([moduleName, moduleData]) => {
-                if (Object.keys(moduleData.structs).length === 0) return;
-                return (
-                  <li key={moduleName} className="relative group">
-                    <div className="px-4 py-2 hover:bg-blue-100 cursor-pointer rounded-xl transition">
-                      {moduleName}
-                    </div>
+    <div className="bg-white p-4 rounded-xl border-2 border-black">
+      <div className="flex items-center gap-4">
+        <div className="inline-block bg-gray-200 text-3xl">Imports</div>
+        <div className="relative py-2">
+          <button
+            onClick={() => setIsOpen((prev) => !prev)}
+            onKeyDown={(e) => {
+              if (e.key === "Escape") setIsOpen(false);
+            }}
+            className="p-2 px-4 rounded-xl bg-blue-500 cursor-pointer hover:bg-blue-600 text-white transition"
+          >
+            ➕ Import 추가
+          </button>
+          {isOpen && (
+            <div className="absolute left-0 p-4 mt-2 w-96 z-50 bg-white rounded-xl shadow overflow-auto max-h-64">
+              <ul className="w-48 bg-white border rounded-xl shadow-lg z-10">
+                {Object.entries(data).map(([moduleName, moduleData]) => {
+                  if (Object.keys(moduleData.structs).length === 0) return;
+                  return (
+                    <li key={moduleName} className="relative group">
+                      <div className="px-4 py-2 hover:bg-blue-100 cursor-pointer rounded-xl transition">
+                        {moduleName}
+                      </div>
 
-                    <ul className="absolute left-full top-0 w-40 bg-white border rounded-xl shadow-lg hidden group-hover:block z-20">
-                      {Object.keys(moduleData.structs).map((structName) => (
-                        <li
-                          key={structName}
-                          onClick={() => {
-                            addImport(moduleName, structName);
-                          }}
-                          className="px-4 py-2 text-emerald-500 hover:bg-blue-50 cursor-pointer transition"
-                        >
-                          {structName}
-                        </li>
-                      ))}
-                    </ul>
-                  </li>
-                );
-              })}
-            </ul>
-          </div>
-        )}
+                      <ul className="absolute left-full top-0 w-40 bg-white border rounded-xl shadow-lg hidden group-hover:block z-20">
+                        {Object.keys(moduleData.structs).map((structName) => (
+                          <li
+                            key={structName}
+                            onClick={() => {
+                              addImport(moduleName, structName);
+                            }}
+                            className="px-4 py-2 text-emerald-500 hover:bg-blue-50 cursor-pointer transition"
+                          >
+                            {structName}
+                          </li>
+                        ))}
+                      </ul>
+                    </li>
+                  );
+                })}
+              </ul>
+            </div>
+          )}
+        </div>
       </div>
+      {Object.entries(imports).map(([key, values]) => {
+        return (
+          <div key={key}>
+            <span className="text-blue-500">use</span> sui::{key}:: &#123;{" "}
+            <span className="text-emerald-500 font-semibold">
+              {Object.keys(values).join(", ")}
+            </span>{" "}
+            &#125;;
+          </div>
+        );
+      })}
     </div>
   );
 }
