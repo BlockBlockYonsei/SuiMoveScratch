966d5cd1515eedb7b1d2551f15811d2b51802e35
ddol9 <drddol9@gmail.com>
2025-05-05 04:11:34 +0900

    Add code download btn

--- src/pages/Final.tsx
+++ src/pages/Final.tsx
@@ -5,6 +5,7 @@ import {
   generateImportsCode,
   generateStructCode,
   generateFunctionCode,
+  downloadMoveCode,
 } from "./NoCodeMove/utils/generateCode";
 
 export default function Final() {
@@ -34,12 +35,25 @@ export default function Final() {
         />
 
         <div className="flex-1 p-5 space-y-6 text-sm font-mono">
+          <button
+            className="mb-4 px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-600"
+            onClick={() => downloadMoveCode(imports, structs, functions)}
+          >
+            code download
+          </button>
+
           <pre className="bg-white p-4 rounded-md shadow whitespace-pre-wrap overflow-auto">
             <code>{generateImportsCode(imports)}</code>
             <br /> <br />
             <code>
               {Object.entries(structs)
-                .map(([name, s]) => generateStructCode(name, s))
+                .map(([name, s]) =>
+                  generateStructCode(
+                    name,
+                    s,
+                    (s as any).typeParameterNames || [],
+                  ),
+                )
                 .join("\n\n")}
             </code>
             <br /> <br />
