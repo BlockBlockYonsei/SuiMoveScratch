ea2ff6d54c0a62112b2d6c8395c2cc6393ad98a2
guboc11 <guboc11@gmail.com>
2025-05-14 17:03:29 +0900

    Split CodePreview from Final page

--- src/pages/NoCodeMoveV2/Final.tsx
+++ src/pages/NoCodeMoveV2/Final.tsx
@@ -1,13 +1,8 @@
 import { AppSidebar } from "./AppSidebar";
 import { SuiMoveNormalizedStruct } from "@mysten/sui/client";
 import { useState } from "react";
-import {
-  generateImportsCode,
-  generateStructCode,
-  generateFunctionCode,
-  downloadMoveCode,
-} from "../NoCodeMove/utils/generateCode";
 import { SuiMoveFunction } from "@/types/move";
+import CodePreview from "./CodePreview";
 
 export default function Final() {
   const [imports, setImports] = useState<
@@ -33,37 +28,11 @@ export default function Final() {
           setStructs={setStructs}
           setFunctions={setFunctions}
         />
-
-        <div className="flex-1 p-5 space-y-6 text-sm font-mono">
-          <button
-            className="mb-4 px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-600"
-            onClick={() => downloadMoveCode(imports, structs, functions)}
-          >
-            code download
-          </button>
-
-          <pre className="bg-white p-4 rounded-md shadow whitespace-pre-wrap overflow-auto">
-            <code>{generateImportsCode(imports)}</code>
-            <br /> <br />
-            <code>
-              {Object.entries(structs)
-                .map(([name, s]) =>
-                  generateStructCode(
-                    name,
-                    s,
-                    (s as any).typeParameterNames || []
-                  )
-                )
-                .join("\n\n")}
-            </code>
-            <br /> <br />
-            <code>
-              {Object.entries(functions)
-                .map(([name, f]) => generateFunctionCode(name, f))
-                .join("\n\n")}
-            </code>
-          </pre>
-        </div>
+        <CodePreview
+          imports={imports}
+          structs={structs}
+          functions={functions}
+        />
       </div>
     </div>
   );
