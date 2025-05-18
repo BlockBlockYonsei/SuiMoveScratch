4815c4809a188cdf677f95e7bfa7384d071fef2f
guboc11 <guboc11@gmail.com>
2025-05-18 20:31:33 +0900

    Update SideBarEditor button size

--- src/pages/NoCodeMoveV2/SideBarEditor.tsx
+++ src/pages/NoCodeMoveV2/SideBarEditor.tsx
@@ -23,54 +23,48 @@ export function SideBarEditor({
       </SidebarHeader>
 
       <SidebarContent>
-        <SidebarGroup>
+        <SidebarGroup className="space-y-1">
           <Button
             variant={menu === "imports" ? "default" : "ghost"}
             className={`${
               menu === "imports"
                 ? "bg-black text-white hover:bg-black/90"
                 : "text-gray-800 hover:bg-gray-100"
-            } cursor-pointer
+            } cursor-pointer py-6 text-lg font-semibold
               `}
             onClick={() => setMenu("imports")}
           >
             Imports
           </Button>
-        </SidebarGroup>
-        <SidebarGroup>
           <Button
             variant={menu === "structs" ? "default" : "ghost"}
             className={`${
               menu === "structs"
                 ? "bg-black text-white hover:bg-black/90"
                 : "text-gray-800 hover:bg-gray-100"
-            } cursor-pointer`}
+            } cursor-pointer py-6 text-lg font-semibold`}
             onClick={() => setMenu("structs")}
           >
             Structs
           </Button>
-        </SidebarGroup>
-        <SidebarGroup>
           <Button
             variant={menu === "functions" ? "default" : "ghost"}
             className={`${
               menu === "functions"
                 ? "bg-black text-white hover:bg-black/90"
                 : "text-gray-800 hover:bg-gray-100"
-            } cursor-pointer`}
+            } cursor-pointer py-6 text-lg font-semibold`}
             onClick={() => setMenu("functions")}
           >
             Functions
           </Button>
-        </SidebarGroup>
-        <SidebarGroup>
           <Button
             variant={menu === "code" ? "default" : "ghost"}
             className={`${
               menu === "code"
                 ? "bg-black text-white hover:bg-black/90"
                 : "text-gray-800 hover:bg-gray-100"
-            } cursor-pointer`}
+            } cursor-pointer py-6 text-lg font-semibold`}
             onClick={() => setMenu("code")}
           >
             Code
