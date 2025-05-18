62f2215548ffe2ba523a6e88a87ab84e327ee39c
guboc11 <guboc11@gmail.com>
2025-05-18 20:21:59 +0900

    Add handleBeforeUnload()

--- src/pages/NoCodeMoveV2/Main.tsx
+++ src/pages/NoCodeMoveV2/Main.tsx
@@ -1,12 +1,25 @@
 import { SideBarEditor } from "@/pages/NoCodeMoveV2/SideBarEditor";
 import CodePreview from "@/pages/NoCodeMoveV2/CodePreview";
 import { SuiMoveModuleProvider } from "@/context/SuiMoveModuleContext";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 export default function Main() {
   const [menu, setMenu] = useState<
     "imports" | "structs" | "functions" | "code"
   >("imports");
+
+  useEffect(() => {
+    const handleBeforeUnload = (e: any) => {
+      e.preventDefault();
+      e.returnValue = ""; // 일부 브라우저에서는 이 설정이 필수
+    };
+
+    window.addEventListener("beforeunload", handleBeforeUnload);
+
+    return () => {
+      window.removeEventListener("beforeunload", handleBeforeUnload);
+    };
+  }, []);
 
   return (
     <div className="w-5/6 min-h-screen m-auto bg-gray-200">
