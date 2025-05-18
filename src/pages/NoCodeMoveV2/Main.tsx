499df13d6266d3f7a6f45073ae5e96634f763bc6
poqopo <block.poqopo@gmail.com>
2025-05-18 15:45:37 +0900

    CSS Change

--- src/pages/NoCodeMoveV2/Main.tsx
+++ src/pages/NoCodeMoveV2/Main.tsx
@@ -1,14 +1,19 @@
 import { SideBarEditor } from "@/pages/NoCodeMoveV2/SideBarEditor";
 import CodePreview from "@/pages/NoCodeMoveV2/CodePreview";
 import { SuiMoveModuleProvider } from "@/context/SuiMoveModuleContext";
+import { useState } from "react";
 
 export default function Main() {
+  const [menu, setMenu] = useState<
+    "imports" | "structs" | "functions" | "code"
+  >("imports");
+
   return (
     <div className="w-5/6 min-h-screen m-auto bg-gray-200">
       <SuiMoveModuleProvider>
         <div className="flex">
-          <SideBarEditor />
-          <CodePreview />
+          <SideBarEditor menu={menu} setMenu={setMenu} />
+          <CodePreview menu={menu} />
         </div>
       </SuiMoveModuleProvider>
     </div>
