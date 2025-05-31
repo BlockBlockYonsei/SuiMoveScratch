f34c9ab5859717fb3518f20802f40d449d733a8b
guboc11 <guboc11@gmail.com>
2025-06-01 04:11:46 +0900

    Update download move package

---
+++ public/sui_move_tests.move
@@ -0,0 +1,18 @@
+/*
+#[test_only]
+module new::new_tests;
+// uncomment this line to import the module
+// use new::module;
+
+const ENotImplemented: u64 = 0;
+
+#[test]
+fun test() {
+    // pass
+}
+
+#[test, expected_failure(abort_code = ::new::new_tests::ENotImplemented)]
+fun test_fail() {
+    abort ENotImplemented
+}
+*/
--- src/pages/NoCodeMoveV2/SideBarEditor.tsx
+++ src/pages/NoCodeMoveV2/SideBarEditor.tsx
@@ -19,82 +19,23 @@ export function SideBarEditor({
   setMenu: (menu: "Imports" | "Structs" | "Functions" | "CodePreview") => void;
   moduleCodes: Record<string, string>;
 }) {
-  const handleDownload = () => {
+  const handleDownload = async () => {
     const zip = new JSZip();
 
     Object.entries(moduleCodes).forEach(([moduleName, moduleCode]) => {
       zip.file(`sui_move/sources/${moduleName}.move`, moduleCode);
     });
 
-    zip.file(
-      `sui_move/tests/sui_move_tests.move`,
-      `/*
-#[test_only]
-module simple_nft::simple_nft_tests;
-// uncomment this line to import the module
-// use simple_nft::simple_nft;
+    const responseTest = await fetch("/sui_move_tests.move");
+    const testFile = await responseTest.text();
 
-const ENotImplemented: u64 = 0;
+    zip.file(`sui_move/tests/sui_move_tests.move`, testFile);
 
-#[test]
-fun test_simple_nft() {
-    // pass
-}
-
-#[test, expected_failure(abort_code = ::simple_nft::simple_nft_tests::ENotImplemented)]
-fun test_simple_nft_fail() {
-    abort ENotImplemented
-}
-*/`
-    );
-
-    const moveTomlContent = `[package]
-name = "new"
-edition = "2024.beta" # edition = "legacy" to use legacy (pre-2024) Move
-# license = ""           # e.g., "MIT", "GPL", "Apache 2.0"
-# authors = ["..."]      # e.g., ["Joe Smith (joesmith@noemail.com)", "John Snow (johnsnow@noemail.com)"]
-
-[dependencies]
-
-# For remote import, use the \`{ git = "...", subdir = "...", rev = "..." }\`.
-# Revision can be a branch, a tag, and a commit hash.
-# MyRemotePackage = { git = "https://some.remote/host.git", subdir = "remote/path", rev = "main" }
-
-# For local dependencies use \`local = path\`. Path is relative to the package root
-# Local = { local = "../path/to" }
-
-# To resolve a version conflict and force a specific version for dependency
-# override use \`override = true\`
-# Override = { local = "../conflicting/version", override = true }
-
-[addresses]
-new = "0x0"
-
-# Named addresses will be accessible in Move as \`@name\`. They're also exported:
-# for example, \`std = "0x1"\` is exported by the Standard Library.
-# alice = "0xA11CE"
-
-[dev-dependencies]
-# The dev-dependencies section allows overriding dependencies for \`--test\` and
-# \`--dev\` modes. You can introduce test-only dependencies here.
-# Local = { local = "../path/to/dev-build" }
-
-[dev-addresses]
-# The dev-addresses section allows overwriting named addresses for the \`--test\`
-# and \`--dev\` modes.
-# alice = "0xB0B"
-`;
+    const responseMoveToml = await fetch("/Move.toml");
+    const moveTomlContent = await responseMoveToml.text();
 
     zip.file(`sui_move/Move.toml`, moveTomlContent);
 
-    // Object.entries(moduleCodes).map(([moduleName, moduleCode]) => {
-    //   const blob = new Blob([moduleCode], { type: "text/plain" });
-    //   const url = URL.createObjectURL(blob);
-    //   const a = document.createElement("a");
-    //   a.href = url;
-    //   a.download = `${moduleName}.move`;
-    //   a.click();
-    // });
     zip.generateAsync({ type: "blob" }).then((blob) => {
       saveAs(blob, "move_package.zip");
     });
