362a7a0a0233f272815562da9c849420d8bc2f42
guboc11 <guboc11@gmail.com>
2025-06-01 12:56:00 +0900

    Split useStructDataHook from StructEditorDialog

--- src/pages/NoCodeMoveV2/structs/StructEditorDialog.tsx
+++ src/pages/NoCodeMoveV2/structs/StructEditorDialog.tsx
@@ -1,16 +1,8 @@
-import { useContext, useEffect, useState } from "react";
+import { useContext } from "react";
 import { X } from "lucide-react";
-import { createHighlighter } from "shiki";
-import {
-  SuiMoveAbility,
-  SuiMoveNormalizedField,
-  SuiMoveNormalizedType,
-  SuiMoveStructTypeParameter,
-} from "@mysten/sui/client";
+import { SuiMoveNormalizedType } from "@mysten/sui/client";
 
-import { SuiMoveStruct } from "@/types/move-type";
 import { SuiMoveModuleContext } from "@/context/SuiMoveModuleContext";
-import { generateStructCode } from "@/lib/generateCode";
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import {
@@ -25,136 +17,23 @@ import AbilitySelector from "../components/AbilitySelector";
 import TypeSelector from "../components/TypeSelector";
 import NewFieldEntityInput from "../components/NewFieldEntityInput";
 import EditableInput from "../components/EditableInput";
+import useStructDataHook from "./useStructDataHook";
 
 export default function StructEditorDialog() {
-  const [previewCode, setPreviewCode] = useState("");
+  const {
+    previewCode,
+    structName,
+    setStructName,
+    abilities,
+    setAbilities,
+    fields,
+    setFields,
+    typeParameters,
+    setTypeParameters,
+    handleComplete,
+  } = useStructDataHook();
 
-  const [structName, setStructName] = useState("NewStruct");
-  const [abilities, setAbilities] = useState<SuiMoveAbility[]>([]);
-  const [typeParameters, setTypeParameters] = useState<
-    { name: string; type: SuiMoveStructTypeParameter }[]
-  >([]);
-  const [fields, setFields] = useState<SuiMoveNormalizedField[]>([]);
-
-  const { moduleName, structs, setStructs, selectedStruct } =
-    useContext(SuiMoveModuleContext);
-
-  useEffect(() => {
-    const createCode = async () => {
-      const highlighter = await createHighlighter({
-        langs: ["move"],
-        themes: ["nord"],
-      });
-      const structCode = generateStructCode({
-        address: "0x0",
-        moduleName: moduleName,
-        structName: structName,
-        abilities: { abilities },
-        fields,
-        typeParameters: typeParameters.map((t) => t.type),
-        typeParameterNames: typeParameters.map((t) => t.name),
-      } as SuiMoveStruct);
-
-      const highlightedCode = highlighter.codeToHtml(structCode, {
-        lang: "move",
-        theme: "nord",
-      });
-
-      setPreviewCode(highlightedCode);
-    };
-
-    createCode();
-  }, [structName, abilities, fields, typeParameters]);
-
-  useEffect(() => {
-    if (selectedStruct) {
-      setStructName(selectedStruct.structName);
-      setAbilities(selectedStruct.abilities.abilities);
-      setTypeParameters(
-        selectedStruct.typeParameters.map((tp, i) => ({
-          name: selectedStruct.typeParameterNames[i],
-          type: tp,
-        }))
-      );
-      setFields(selectedStruct.fields);
-    } else {
-      resetState();
-    }
-  }, [selectedStruct, structs]);
-
-  const handleComplete = () => {
-    if (!structName) return;
-
-    setStructs((prev) => {
-      const newStructMap = new Map(prev);
-
-      const newStructData: SuiMoveStruct = {
-        address: "0x0",
-        moduleName: moduleName,
-        structName: structName,
-        abilities: { abilities },
-        fields,
-        typeParameters: typeParameters.map((t) => t.type),
-        typeParameterNames: typeParameters.map((t) => t.name),
-      };
-
-      if (!selectedStruct) {
-        newStructMap.set(structName, newStructData);
-        return newStructMap;
-      }
-
-      if (selectedStruct.structName === structName) {
-        newStructMap.set(structName, newStructData);
-        return newStructMap;
-      }
-
-      // 이전 struct 이름이 있고, 새로운 이름과 다른 경우 (이름 변경)
-      newStructMap.delete(selectedStruct.structName);
-      newStructMap.set(structName, newStructData);
-
-      // 다른 Struct Field에 해당 Struct가 쓰였다면 함께 이름 변경해줌
-      newStructMap.forEach((structData) => {
-        const updatedFields = structData.fields.map((field) => {
-          if (
-            field.type &&
-            typeof field.type === "object" &&
-            "Struct" in field.type &&
-            field.type.Struct.name === selectedStruct.structName
-          ) {
-            const updatedField: SuiMoveNormalizedField = {
-              name: field.name,
-              type: {
-                Struct: {
-                  ...field.type.Struct,
-                  name: structName,
-                },
-              },
-            };
-            return updatedField;
-          }
-
-          return field;
-        });
-
-        newStructMap.set(structData.structName, {
-          ...structData,
-          fields: updatedFields,
-        } as SuiMoveStruct);
-      });
-
-      return newStructMap;
-    });
-
-    resetState();
-  };
-
-  const resetState = () => {
-    setStructName("NewStruct");
-    setAbilities([]);
-    setTypeParameters([]);
-    setFields([]);
-  };
-
+  const { selectedStruct } = useContext(SuiMoveModuleContext);
   return (
     <DialogContent className="sm:max-w-[600px] lg:max-w-[1000px] max-h-[80vh] overflow-y-auto">
       <DialogHeader>
@@ -197,19 +76,6 @@ export default function StructEditorDialog() {
 
           {/* Type Parameters */}
           <div className="mb-4">
-            {/* <NewTypeParameterInput
-              onCreate={(name) => {
-                if (typeParameters.map((t) => t.name).includes(name)) return;
-
-                setTypeParameters([
-                  ...typeParameters,
-                  {
-                    name,
-                    type: { isPhantom: false, constraints: { abilities: [] } },
-                  },
-                ]);
-              }}
-            /> */}
             <NewFieldEntityInput
               title="Type Parameter"
               filter={(value: string) => {
---
+++ src/pages/NoCodeMoveV2/structs/useStructDataHook.tsx
@@ -0,0 +1,154 @@
+import { useContext, useEffect, useState } from "react";
+import { createHighlighter } from "shiki";
+import {
+  SuiMoveAbility,
+  SuiMoveNormalizedField,
+  SuiMoveStructTypeParameter,
+} from "@mysten/sui/client";
+
+import { SuiMoveStruct } from "@/types/move-type";
+import { SuiMoveModuleContext } from "@/context/SuiMoveModuleContext";
+import { generateStructCode } from "@/lib/generateCode";
+
+export default function useStructDataHook() {
+  const [previewCode, setPreviewCode] = useState("");
+
+  const [structName, setStructName] = useState("NewStruct");
+  const [abilities, setAbilities] = useState<SuiMoveAbility[]>([]);
+  const [typeParameters, setTypeParameters] = useState<
+    { name: string; type: SuiMoveStructTypeParameter }[]
+  >([]);
+  const [fields, setFields] = useState<SuiMoveNormalizedField[]>([]);
+
+  const { moduleName, structs, setStructs, selectedStruct } =
+    useContext(SuiMoveModuleContext);
+
+  useEffect(() => {
+    const createCode = async () => {
+      const highlighter = await createHighlighter({
+        langs: ["move"],
+        themes: ["nord"],
+      });
+      const structCode = generateStructCode({
+        address: "0x0",
+        moduleName: moduleName,
+        structName: structName,
+        abilities: { abilities },
+        fields,
+        typeParameters: typeParameters.map((t) => t.type),
+        typeParameterNames: typeParameters.map((t) => t.name),
+      } as SuiMoveStruct);
+
+      const highlightedCode = highlighter.codeToHtml(structCode, {
+        lang: "move",
+        theme: "nord",
+      });
+
+      setPreviewCode(highlightedCode);
+    };
+
+    createCode();
+  }, [structName, abilities, fields, typeParameters]);
+
+  useEffect(() => {
+    if (selectedStruct) {
+      setStructName(selectedStruct.structName);
+      setAbilities(selectedStruct.abilities.abilities);
+      setTypeParameters(
+        selectedStruct.typeParameters.map((tp, i) => ({
+          name: selectedStruct.typeParameterNames[i],
+          type: tp,
+        }))
+      );
+      setFields(selectedStruct.fields);
+    } else {
+      resetState();
+    }
+  }, [selectedStruct, structs]);
+
+  const handleComplete = () => {
+    if (!structName) return;
+
+    setStructs((prev) => {
+      const newStructMap = new Map(prev);
+
+      const newStructData: SuiMoveStruct = {
+        address: "0x0",
+        moduleName: moduleName,
+        structName: structName,
+        abilities: { abilities },
+        fields,
+        typeParameters: typeParameters.map((t) => t.type),
+        typeParameterNames: typeParameters.map((t) => t.name),
+      };
+
+      if (!selectedStruct) {
+        newStructMap.set(structName, newStructData);
+        return newStructMap;
+      }
+
+      if (selectedStruct.structName === structName) {
+        newStructMap.set(structName, newStructData);
+        return newStructMap;
+      }
+
+      // 이전 struct 이름이 있고, 새로운 이름과 다른 경우 (이름 변경)
+      newStructMap.delete(selectedStruct.structName);
+      newStructMap.set(structName, newStructData);
+
+      // 다른 Struct Field에 해당 Struct가 쓰였다면 함께 이름 변경해줌
+      newStructMap.forEach((structData) => {
+        const updatedFields = structData.fields.map((field) => {
+          if (
+            field.type &&
+            typeof field.type === "object" &&
+            "Struct" in field.type &&
+            field.type.Struct.name === selectedStruct.structName
+          ) {
+            const updatedField: SuiMoveNormalizedField = {
+              name: field.name,
+              type: {
+                Struct: {
+                  ...field.type.Struct,
+                  name: structName,
+                },
+              },
+            };
+            return updatedField;
+          }
+
+          return field;
+        });
+
+        newStructMap.set(structData.structName, {
+          ...structData,
+          fields: updatedFields,
+        } as SuiMoveStruct);
+      });
+
+      return newStructMap;
+    });
+
+    resetState();
+  };
+
+  const resetState = () => {
+    setStructName("NewStruct");
+    setAbilities([]);
+    setTypeParameters([]);
+    setFields([]);
+  };
+  return {
+    previewCode,
+    setPreviewCode,
+    structName,
+    setStructName,
+    abilities,
+    setAbilities,
+    fields,
+    setFields,
+    typeParameters,
+    setTypeParameters,
+    handleComplete,
+  };
+}
