5503a9443d9541fa45b160ec7b5d4da54399164f
guboc11 <guboc11@gmail.com>
2025-06-02 03:38:52 +0900

    Update handleComplete in StructDataHook

--- src/pages/NoCodeMoveV2/structs/useStructDataHook.tsx
+++ src/pages/NoCodeMoveV2/structs/useStructDataHook.tsx
@@ -13,6 +13,7 @@ import { generateStructCode } from "@/lib/generateCode";
 export default function useStructDataHook() {
   const [previewCode, setPreviewCode] = useState("");
 
+  const [oldStructName, setOldStructName] = useState("");
   const [structName, setStructName] = useState("NewStruct");
   const [abilities, setAbilities] = useState<SuiMoveAbility[]>([]);
   const [typeParameters, setTypeParameters] = useState<
@@ -54,6 +55,7 @@ export default function useStructDataHook() {
   useEffect(() => {
     if (selectedStruct) {
       setStructName(selectedStruct.structName);
+      setOldStructName(selectedStruct.structName);
       setAbilities(selectedStruct.abilities.abilities);
       setTypeParameters(
         selectedStruct.typeParameters.map((tp, i) => ({
@@ -68,66 +70,71 @@ export default function useStructDataHook() {
   }, [selectedStruct, structs]);
 
   const handleComplete = () => {
-    if (!structName) return;
+    if (!structName) return resetState();
+
+    if (!selectedStruct && structs.has(structName)) return resetState();
+
+    const newStructData: SuiMoveStruct = {
+      address: "0x0",
+      moduleName: moduleName,
+      structName: structName,
+      abilities: { abilities },
+      fields,
+      typeParameters: typeParameters.map((t) => t.type),
+      typeParameterNames: typeParameters.map((t) => t.name),
+    };
 
     setStructs((prev) => {
-      const newStructMap = new Map(prev);
-
-      const newStructData: SuiMoveStruct = {
-        address: "0x0",
-        moduleName: moduleName,
-        structName: structName,
-        abilities: { abilities },
-        fields,
-        typeParameters: typeParameters.map((t) => t.type),
-        typeParameterNames: typeParameters.map((t) => t.name),
-      };
+      const newStructMap = new Map<string, SuiMoveStruct>();
 
       if (!selectedStruct) {
+        [...prev.entries()].forEach(([strtName, strtData]) => {
+          newStructMap.set(strtName, strtData);
+        });
         newStructMap.set(structName, newStructData);
         return newStructMap;
-      }
-
-      if (selectedStruct.structName === structName) {
-        newStructMap.set(structName, newStructData);
-        return newStructMap;
-      }
-
-      // 이전 struct 이름이 있고, 새로운 이름과 다른 경우 (이름 변경)
-      newStructMap.delete(selectedStruct.structName);
-      newStructMap.set(structName, newStructData);
-
-      // 다른 Struct Field에 해당 Struct가 쓰였다면 함께 이름 변경해줌
-      newStructMap.forEach((structData) => {
-        const updatedFields = structData.fields.map((field) => {
-          if (
-            field.type &&
-            typeof field.type === "object" &&
-            "Struct" in field.type &&
-            field.type.Struct.name === selectedStruct.structName
-          ) {
-            const updatedField: SuiMoveNormalizedField = {
-              name: field.name,
-              type: {
-                Struct: {
-                  ...field.type.Struct,
-                  name: structName,
-                },
-              },
-            };
-            return updatedField;
+      } else {
+        // 이전 struct 이름이 있고, 새로운 이름과 다른 경우 (이름 변경)
+        [...prev.entries()].forEach(([strtName, strtData]) => {
+          if (strtName === oldStructName) {
+            newStructMap.set(structName, newStructData);
+            return;
           }
-
-          return field;
+          newStructMap.set(strtName, strtData);
         });
 
-        newStructMap.set(structData.structName, {
-          ...structData,
-          fields: updatedFields,
-        } as SuiMoveStruct);
-      });
+        // 다른 Struct Field에 해당 Struct가 쓰였다면 함께 이름 변경해줌
+        newStructMap.forEach((structData) => {
+          const updatedFields = structData.fields.map((field) => {
+            if (
+              field.type &&
+              typeof field.type === "object" &&
+              "Struct" in field.type &&
+              field.type.Struct.name === oldStructName
+            ) {
+              const updatedField: SuiMoveNormalizedField = {
+                name: field.name,
+                type: {
+                  Struct: {
+                    ...field.type.Struct,
+                    name: structName,
+                  },
+                },
+              };
+              return updatedField;
+            }
 
-      return newStructMap;
+            return field;
+          });
+
+          newStructMap.set(structData.structName, {
+            ...structData,
+            fields: updatedFields,
+          } as SuiMoveStruct);
+        });
+
+        return newStructMap;
+      }
     });
 
     resetState();
